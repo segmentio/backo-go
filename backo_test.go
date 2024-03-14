11fe77d897d9bfd79aec8e67400dd8e7276cf819
Erik Weathers <erik.weathers@segment.com>
2024-03-13 17:52:13 -0700

    switch to testify/assert; remove undocumented gitmodules stuff & use go modules more normally

--- backo_test.go
+++ backo_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/bmizerany/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 // Tests default backo behaviour.
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/segmentio/backo-go
 
 go 1.13
+
+require github.com/stretchr/testify v1.9.0 // indirect
